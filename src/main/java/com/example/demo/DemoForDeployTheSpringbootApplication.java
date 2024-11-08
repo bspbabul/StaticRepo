e6b035e41b93a28e09fbd990077551f4629f7b51
Babul Kumar Sahu <sahu15babul@gmail.com>
2024-11-08 14:53:03 +0530

    my first commit

---
+++ src/main/java/com/example/demo/DemoForDeployTheSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForDeployTheSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForDeployTheSpringbootApplication.class, args);
+	}
+
+}
